839527e22331c19ce86f917c416d94e05ea61f94
Magnus Persson <Magnus.Persson@ipeer.se>
2018-02-07 14:49:36 +0100

    Added visualizer

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -1,1 +1,6 @@
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,source=/Users/nergal/Dropbox/Work/tech_days_docker/code/jenkins,target=/var/jenkins_home --name Jenkins -p 8080:8080 --rm -it jenkins:ver1
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock \
+    --mount type=bind,source=/Users/nergal/Dropbox/Work/tech_days_docker/code/jenkins,target=/var/jenkins_home \
+    -e HOST_IP=`ifconfig |grep inet |grep broadcast|awk '{print $2}'`\
+    --name Jenkins \
+    -p 8080:8080 \
+    --rm -it jenkins:ver1
