fc892e532017c46ecf73c191839142a54221a4a8
Magnus Persson <Magnus.Persson@ipeer.se>
2018-02-07 15:08:34 +0100

    Added scope

--- start_visualizer.sh
+++ start_visualizer.sh
@@ -1,1 +1,1 @@
-docker run -it -d -p 8081:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualize
+docker run --name visualizer --rm -it -d -p 8081:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualizer
